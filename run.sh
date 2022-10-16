f55bbfdd6a4a9fe14f3deb397345dd4c350bbf8f
diatools <111637249+diatools@users.noreply.github.com>
2022-10-16 15:50:25 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 
-curl -sSL get.docker.com | bash -E
-apt install -y docker-compose
+service ufw stop
 
 mkdir -p $HOME/nightscout
 cd $HOME/nightscout
+if [-f ".env"]; then
+        echo "\nGeben Sie die DNS-Namen des Servers ein:"
+        read HOST
+
+        echo "\nGeben Sie Ihre E-Mail Adresse ein:"
+        read EMAIL
+
+        echo "\nLegen Sie ein Passwort (API-Secret) für Nightsout fest:"
+        read SECRET
+
+        echo "HOST=$HOST" > .env
+        echo "EMAIL=$EMAIL" >> .env
+        echo "SECRET=$SECRET" >> .env
+
+        echo "\n\nDie eingegebenen informationen wurden nach $HOME/nightscout/.env geschrieben:"
+else
+        echo "\n\nDie informationen aus $HOME/nightscout/.env wedern verwendet:"
+fi
+
+echo "\n****************************\n"
+cat .env
+echo "\n****************************\n"
+
+curl -sSL get.docker.com | bash -E
+apt install -y docker-compose
+
 curl -sSL https://raw.githubusercontent.com/diatools/nightscout-ionos/main/docker-compse.yml > docker-compose.yml
 
 docker-compose up -d --remove-orphans
